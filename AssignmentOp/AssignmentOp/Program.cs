767f62551baae9a9cdd0c3cee11cbb838482c330
Jorge Eldis <73083341+jorgeeldis@users.noreply.github.com>
2021-01-08 19:30:32 -0500

    Add files via upload

---
+++ AssignmentOp/AssignmentOp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AssignmentOp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 42;
+            int y = 10;
+            int z = 3;
+            int v = ++z;
+            int w = z++;
+            x += 2;
+            x -= 6;
+            x *= 8;
+            x /= 5;
+            x %= 2;
+            y++;
+            Console.WriteLine(y);
+            Console.WriteLine(v);
+            Console.WriteLine(w);
+        }
+    }
+}
---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 15;
+            int y = 6;
+            x %= y;
+            Console.WriteLine(x);
+        }
+    }
+}
