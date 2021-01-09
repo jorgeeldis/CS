d21636ca697bd0690184b4f3d2988a4f77c72d28
Jorge Eldis <73083341+jorgeeldis@users.noreply.github.com>
2021-01-08 19:35:42 -0500

    Add files via upload

---
+++ TheVarKeyword/TheVarKeyword/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TheVarKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n1;
+            n1 = 42;
+            Console.WriteLine(n1);
+            
+        }
+    }
+}
