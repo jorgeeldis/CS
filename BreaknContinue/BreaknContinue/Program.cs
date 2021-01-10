4113b57822ef2286f7056ec0361243a781b53927
Jorge Eldis <73083341+jorgeeldis@users.noreply.github.com>
2021-01-09 22:54:47 -0500

    Add files via upload

---
+++ BreaknContinue/BreaknContinue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BreaknContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            while (num < 20)
+            {
+                if (num == 5)
+                    break;
+
+                Console.WriteLine(num);
+                num++;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                    continue;
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
