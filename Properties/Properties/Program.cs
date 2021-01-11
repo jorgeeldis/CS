51c3f5f64d26c163a41b5c996e5e6efd6b80c854
Jorge Eldis <73083341+jorgeeldis@users.noreply.github.com>
2021-01-11 18:06:09 -0500

    Add files via upload

---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+
+        class Person
+        {
+            public string Name
+            {
+                get; set;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Name = "Bob";
+            Console.WriteLine(p.Name);
+        }
+    }
+}
