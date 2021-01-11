5397b2bb47eeb27bced7dcd19eb2d002fe05c2a3
Jorge Eldis <73083341+jorgeeldis@users.noreply.github.com>
2021-01-11 18:03:48 -0500

    Add files via upload

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        class BankAccount
+        {
+            private double balance = 0;
+            public void Deposit(double n)
+            {
+                balance += n;
+            }
+            public void Withdraw(double n)
+            {
+                balance -= n;
+            }
+            public double GetBalance()
+            {
+                return balance;
+            }
+        }
+        static void Main(string[] args)
+        {
+            BankAccount b = new BankAccount();
+            b.Deposit(199);
+            b.Withdraw(42);
+            Console.WriteLine(b.GetBalance());
+        }
+    }
+}
