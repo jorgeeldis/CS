e1a358562659dd77263473e9de1639ad591522b7
Jorge Eldis <73083341+jorgeeldis@users.noreply.github.com>
2021-01-12 21:41:48 -0500

    Add files via upload

---
+++ ArrayP/ArrayP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 6, 5, 2 };
+            Console.WriteLine(arr.Max());
+            Console.WriteLine(arr.Min());
+            Console.WriteLine(arr.Sum());
+        }
+    }
+}
