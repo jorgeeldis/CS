3c4d8ee1f3f92ba2a3d4b4e8c03029776fd0aef4
Jorge Eldis <73083341+jorgeeldis@users.noreply.github.com>
2021-01-13 20:58:59 -0500

    Add files via upload

---
+++ Destructor/Destructor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Destructor
+{
+    class Program
+    {
+        class Dog
+        {
+            public Dog()
+            {
+                Console.WriteLine("constructor");
+            }
+            ~Dog()
+            {
+                Console.WriteLine("destructor");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+        }
+    }
+}
